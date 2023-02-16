07b7783394acc1abf67423c3f09b54297092fd7a
Adrian Bach <adrian.bach@stir.ac.uk>
2023-02-16 18:47:32 +0000

    res stat

---
+++ MergeStatsPlot.R
@@ -0,0 +1,11 @@
+Path = "C:/Users/adb3/Desktop/PhD/GitKraken/Chapter3sim/"
+folder = "C:/Users/adb3/Desktop/PhD/GitKraken/Chapter3sim/folder-preyAloneCtchPr/"
+Keyword = "Results"
+Pattern = "newSA-p"
+
+path = Path; keyword = Keyword; pattern = Pattern
+
+mergeResults(path = Path, keyword = Keyword, pattern = Pattern)
+
+statsResults(path = Path, keyword = Keyword, pattern = Pattern)
+# statsResultsNoExt(path = Path, keyword = Keyword, pattern = Pattern, exclude.ext = T)
