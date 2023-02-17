9155b8e6da2ec38bc7d7f82e70d98b02b1d5c65f
Adrian Bach <adrian.bach@stir.ac.uk>
2023-02-17 15:42:15 +0000

    encore

--- folder-introIIprey/introIIprey-launchReplicatesSim.sh
+++ folder-introIIprey/introIIprey-launchReplicatesSim.sh
@@ -21,8 +21,8 @@
 # landscape variables
 size=25       # argv[2] world's side size
 res_nb=2     # argv[3] number of resource types
-max_res_1=100 # argv[4] max resource 1 per cell
-max_res_2=100 # argv[5] max resource 2 per cell
+max_res_1=1000 # argv[4] max resource 1 per cell
+max_res_2=1000 # argv[5] max resource 2 per cell
 
 # prey variables
 pry_nb=2        # argv[6] number of prey types
@@ -30,13 +30,13 @@ pry_init_1=100  # argv[7] prey 1 initial density in nb of individuals
 pry_init_2=100  # argv[8] prey 2 initial density
 pry_move_1=0.1  # argv[9] prey 1 max movement range in fraction of size
 pry_move_2=0.1  # argv[10] prey 2 movement range
-pry_cons_1=5	# argv[11] prey 1 max consumption in resource units
-pry_cons_2=5	# argv[12] prey 2 max consumption in resource units
-pry_surv_1=20  # argv[13] prey 1 resource units needed to pass survival trial
-pry_surv_2=20  # argv[14] prey 2 resource units needed to pass survival trial
-pry_offs_1=2    # argv[15] prey 1 max number of offspring
+pry_cons_1=10	# argv[11] prey 1 max consumption in resource units
+pry_cons_2=10	# argv[12] prey 2 max consumption in resource units
+pry_surv_1=30  # argv[13] prey 1 resource units needed to pass survival trial
+pry_surv_2=30  # argv[14] prey 2 resource units needed to pass survival trial
+pry_offs_1=1    # argv[15] prey 1 max number of offspring
 pry_offs_2=1    # argv[16] prey 2 max number of offspring
-# pry_repr_1=5  # argv[17] prey 1 resource units needed to pass reproduction trial
+pry_repr_1=20  # argv[17] prey 1 resource units needed to pass reproduction trial
 pry_repr_2=$pry_surv_2  # argv[18] prey 2 resource units needed to pass reproduction trial
 pry_intro_1=1001   # argv[19]
 pry_intro_2=0   # argv[20]
@@ -45,10 +45,12 @@ pry_intro_2=0   # argv[20]
 prd_nb=1        # argv[21] number of predator types
 prd_init_1=100  # argv[22] predator 1 initial density in nb of individuals
 prd_move_1=0.1  # argv[23] predator 1 max movement range in fraction of size
-prd_surv_1=300	# arg[25]
+prd_cons_1=100   # argv[24] predator 1 satiation level in resource units
+prd_surv_1=300	# arg[25] predator 1 maintenance cost in resource units
 prd_offs_1=0.5    # argv[26] predator 1 max number of offspring
+prd_repr_1=300 # argv[27] predator 1 resource units needed to pass reproduction trial. 
 prd_intr_1=201   # argv[28] predator 1 time of introduction in the model
-# prd_ctch_pry1_1=0.1  # argv[29] predator 1 prey1 catch probability
+prd_ctch_pry1_1=0.1  # argv[29] predator 1 prey1 catch probability
 prd_ctch_pry2_1=0.1  # argv[30] predator 1 prey2 catch probability
 prd_cvrt_pry1_1=100  # argv[31] predator 1 prey1 resources/catch
 prd_cvrt_pry2_1=100  # argv[32] predator 1 prey1 resources/catch
@@ -82,9 +84,9 @@ rep=3
 # pry_repr_1=$(($pry_surv_1/1)); # argv[17] prey 1 resource units needed to pass reproduction trial. Defined as a proportion of what is needed to pass survival trial.
 # pry_repr_2=$(($pry_surv_2/1)); # argv[18] prey 2 resource units needed to pass reproduction trial
 
-divide=$((3*$prd_surv_1)); by=$freq_surv; prd_cons_1=`echo "scale=0; ($divide+$by-1)/$by" | bc`; # arg[24]
+# divide=$((3*$prd_surv_1)); by=$freq_surv; prd_cons_1=`echo "scale=0; ($divide+$by-1)/$by" | bc`; # arg[24]
 # divide=$prd_cons_1*$freq_surv; by=3; prd_surv_1=`echo "scale=0; ($divide+$by-1)/$by" | bc`; # arg[25]
-prd_repr_1=$(($prd_surv_1/1)); # echo "prd_repr_1 = $prd_repr_1" # argv[27] predator 1 resource units needed to pass reproduction trial. Defined as a proportion of what is needed to pass survival trial.
+# prd_repr_1=$(($prd_surv_1/1)); # echo "prd_repr_1 = $prd_repr_1" # argv[27] predator 1 resource units needed to pass reproduction trial. Defined as a proportion of what is needed to pass survival trial.
 # prd_cvrt_pry1_1=$(($freq_surv * $prd_cons_1/3))  # argv[31] predator 1 prey1 resources/catch
 # ratio=1
 # prd_cvrt_pry2_1=$(($ratio*$prd_cvrt_pry1_1))  # argv[32] predator 1 prey1 resources/catch
@@ -95,26 +97,26 @@ make
 
 #### Simulation loop ####
 
-# sim values arrays
-pry1rpdArray=(10 15 20)
-pry1cPrArray=(0.1 0.15 0.20)
+# # sim values arrays
+# pry1rpdArray=(10 15 20)
+# pry1cPrArray=(0.1 0.15 0.20)
 
-echo "pry1rpdArray is ${pry1rpdArray[*]}"
-echo "pry1rpdArray size is ${#pry1rpdArray[@]}"
+# echo "pry1rpdArray is ${pry1rpdArray[*]}"
+# echo "pry1rpdArray size is ${#pry1rpdArray[@]}"
 
-echo "pry1cPrArray is ${pry1cPrArray[*]}"
-echo "pry1cPrArray size is ${#pry1cPrArray[@]}"
+# echo "pry1cPrArray is ${pry1cPrArray[*]}"
+# echo "pry1cPrArray size is ${#pry1cPrArray[@]}"
 
-# loop over pry2resArray
-for ((n=0 ; n<${#pry1rpdArray[@]} ; n++))
-do 
-    pry_repr_1=${pry1rpdArray[$n]}
-    # pry_repr_2=$pry_surv_2
+# # loop over pry2resArray
+# for ((n=0 ; n<${#pry1rpdArray[@]} ; n++))
+# do 
+#     pry_repr_1=${pry1rpdArray[$n]}
+#     # pry_repr_2=$pry_surv_2
 
-    # loop over prdCatchProb
-    for ((i=0 ; i<${#pry1cPrArray[@]} ; i++))
-    do
-        prd_ctch_pry1_1=${pry1cPrArray[$i]}
+#     # loop over prdCatchProb
+#     for ((i=0 ; i<${#pry1cPrArray[@]} ; i++))
+#     do
+#         prd_ctch_pry1_1=${pry1cPrArray[$i]}
 
         # # loop over predOffsArray
         # for ((m=0 ; m<${#predOffsArray[@]} ; m++))
@@ -129,7 +131,7 @@ do
             #     divide=$((3*$prd_surv_1)); by=$freq_surv; prd_cons_1=`echo "scale=0; ($divide+$by-1)/$by" | bc`;
 
                 # name the simulation with only the variables of interest and their value
-                sim_name="introiiPrey-py1repr$pry_repr_1-py1ctPr$prd_ctch_pry1_1" # argv[1]
+                sim_name="introiiPrey-py1repr$pry_repr_1-py2repr$pry_repr_2" # argv[1]
 
                 echo "sim $sim_name"
 
@@ -294,7 +296,7 @@ do
 
             # done
         # done
-    done
-done
+    # done
+# done
 
 echo "end of simulation loop."
