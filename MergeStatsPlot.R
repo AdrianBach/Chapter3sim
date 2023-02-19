6a189de04063a82faba7cdac5fa6d782810c9ad6
Adrian Bach <adrian.bach@stir.ac.uk>
2023-02-19 18:23:06 +0000

    sim folder for cull tests

--- MergeStatsPlot.R
+++ MergeStatsPlot.R
@@ -1,3 +1,45 @@
+library(ggplot2)
+library(stringr)
+
+boot_sd_ci <- function(x, confidence = 95, itr = 1000) {
+  
+  # init iterations and sample
+  i <- itr
+  bt_avg <- NULL
+  
+  # loop over iterations
+  while (i > 0) {
+    # sample randomly from x
+    spl <- sample(x, length(x), replace = TRUE)
+    
+    # store mean
+    bt_avg <- c(bt_avg, sum(spl)/length(spl))
+    
+    # decrement i
+    i <- i-1
+  }
+  
+  # mean over the bootstrapped samples
+  bt_est <- sum(bt_avg)/itr
+  
+  # compute standard deviation
+  bt_sd <- sqrt((1/(length(x)-1)) * sum((bt_avg-bt_est)^2))
+  
+  # compute confidence interval
+  # sorting bt_avg numerically
+  st_avg <- sort(bt_avg)
+  
+  # get the first value after the 2.5 first centiles
+  bt_95ci_inf <- st_avg[floor(0.5*(1-0.01*confidence)*itr)+1]
+  
+  # get the last value before the 2.5 last centiles
+  bt_95ci_sup <- st_avg[floor((0.01*confidence+0.5*(1-0.01*confidence))*itr)-1]
+  
+  res <- c(bt_sd, bt_95ci_inf, bt_95ci_sup)
+  return(res) 
+  
+}
+
 Path = "C:/Users/adb3/Desktop/PhD/GitKraken/Chapter3sim/"
 folder = "C:/Users/adb3/Desktop/PhD/GitKraken/Chapter3sim/folder-preyAlone-newPredMaxCons/"
 Keyword = "Results"
@@ -141,7 +183,7 @@ statsResults <- function(path, keyword = c("Results", "Snapshot"), pattern, freq
     simFol <- grep(pattern = c(pattern), x = simFol, value = T)
     
     # loop over the sim folders
-    for (j in 2:length(simFol)) {
+    for (j in 1:length(simFol)) {
       
       print(paste("in sim folder ", simFol[j]))
       
@@ -293,7 +335,7 @@ statsResults <- function(path, keyword = c("Results", "Snapshot"), pattern, freq
       # plot and save density figure
       data <- tab
       
-      x = data$timeStep
+      x = data$timeStep/10
       y1 = data$prey1PopulationSizeMean
       y2 = data$prey2PopulationSizeMean
       y3 = data$predator1PopulationSizeMean
@@ -319,7 +361,7 @@ statsResults <- function(path, keyword = c("Results", "Snapshot"), pattern, freq
         # geom_point(aes(y = y1), size = 2.5, shape = 21, fill = "white", color = y1c) +
         # geom_point(aes(y = y2), size = 2.5, shape = 22, fill = "white", color = y2c) +
         # geom_point(aes(y = y3), size = 2.5, shape = 24, fill = "white", color = y3c) +
-        labs(x = "Time steps", y = "Population density") +
+        labs(x = "Generations", y = "Population density") +
         scale_colour_manual(name='Populations',
                             breaks=c('Prey 1', 'Prey 2', 'Predator'),
                             values=c(y1c, y2c, y3c))
@@ -333,7 +375,7 @@ statsResults <- function(path, keyword = c("Results", "Snapshot"), pattern, freq
       # plot and save catch rate figure
       # data <- read.csv(paste(statsFol, "/stats", keyword, "-", simFol[j], ".csv", sep = ""))
 
-      x = data$timeStep
+      x = data$timeStep/10
       y1 = data$prey1catchRateMean
       y2 = data$prey2catchRateMean
       # y3 = data$predatorCatchRateMean
@@ -359,7 +401,7 @@ statsResults <- function(path, keyword = c("Results", "Snapshot"), pattern, freq
         # geom_point(aes(y = y1), size = 2.5, shape = 21, fill = "white", color = y1c) +
         # geom_point(aes(y = y2), size = 2.5, shape = 22, fill = "white", color = y2c) +
         # geom_point(aes(y = y3), size = 2.5, shape = 24, fill = "white", color = y3c) +
-        labs(x = "Time steps", y = "Average catch rate between trials") +
+        labs(x = "Generations", y = "Average catch rate between trials") +
         scale_colour_manual(name='Populations',
                             breaks=c('Prey 1', 'Prey 2', 'Predator'),
                             values=c(y1c, y2c, y3c))
